b78ba6b212d0748ec9e24837f1ea6a4ad98e6764
gabdissa <abdissa.galaan@gmail.com>
2019-06-01 21:33:10 -0400

    Added Datastore Logic

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -52,15 +52,15 @@ public class Datastore {
    * @return a list of messages posted by the user, or empty list if user has never posted a
    *     message. List is sorted by time descending.
    */
+
+  List<Message> messages = new ArrayList<>();
+  Query query = new Query("Message").addSort("timestamp", SortDirection.DESCENDING);
+  PreparedQuery results = datastore.prepare(query);
+
   public List<Message> getMessages(String user) {
-    List<Message> messages = new ArrayList<>();
-
-    Query query =
-        new Query("Message")
-            .setFilter(new Query.FilterPredicate("user", FilterOperator.EQUAL, user))
-            .addSort("timestamp", SortDirection.DESCENDING);
-    PreparedQuery results = datastore.prepare(query);
-
+    query.setFilter(new Query.FilterPredicate("user", FilterOperator.EQUAL, user));
+    
+    /** THIS TEST CODE IS NOT NEEDED SINCE getAllMessages() checks these conditions
     for (Entity entity : results.asIterable()) {
       try {
         String idString = entity.getKey().getName();
@@ -76,7 +76,28 @@ public class Datastore {
         e.printStackTrace();
       }
     }
+     */
 
     return messages;
   }
+  public List<Message> getAllMessages(){
+
+    for (Entity entity : results.asIterable()) {
+      try {
+        String idString = entity.getKey().getName();
+        UUID id = UUID.fromString(idString);
+        String user = (String) entity.getProperty("user");
+        String text = (String) entity.getProperty("text");
+        long timestamp = (long) entity.getProperty("timestamp");
+
+        Message message = new Message(id, user, text, timestamp);
+        messages.add(message);
+      } catch (Exception e) {
+        System.err.println("Error reading message.");
+        System.err.println(entity.toString());
+        e.printStackTrace();
+      }
+    }
+  return messages;
+ }
 }
